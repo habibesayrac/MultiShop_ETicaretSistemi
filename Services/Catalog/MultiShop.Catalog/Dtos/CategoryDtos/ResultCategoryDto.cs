3cba78b949f944563db51d8bd9e1c2653d0cb8f5
habibe.sayrac <habibe.sayrac@detaysoft.com>
2024-12-04 19:54:12 +0300

    DtoLayer & Automapper added

---
+++ Services/Catalog/MultiShop.Catalog/Dtos/CategoryDtos/ResultCategoryDto.cs
@@ -0,0 +1,11 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace MultiShop.Catalog.Dtos.CategoryDtos
+{
+    public class ResultCategoryDto
+    {
+        public string CategoryId { get; set; }
+        public string CategoryName { get; set; }
+    }
+}
